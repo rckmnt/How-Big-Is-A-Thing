183b9aa7ed520d9bdc1bdacaff952af09950959f
commissionergordon <scottl318@gmail.com>
2018-04-16 08:35:05 -0400

    starting of textblob to real app

--- textblob_test.py
+++ textblob_test.py
@@ -5,14 +5,36 @@ blahs = ['golden_retriever', 'greenhouse', 'polo_mallet', 'paperboy',
         'pluto', 'quartzffsite', 'powder_keg','power_drill']
 
 
-for b in blahs:
-    word = Word(b)
+# for b in blahs:
+#     word = Word(b)
+#     if word.synsets:
+#         plant = word.synsets[0]
+#         print('{} exists: {}'.format(b, word.synsets))
+#         print('\t\t {}'.format(plant.hypernyms()))
+#     else:
+#         print('{} is NOT a word'.format(b))
+
+
+def get_hypernym(askedfor):
+    # put in a word, get a list of other words
+    word = Word(askedfor)
     if word.synsets:
         plant = word.synsets[0]
-        print('{} exists: {}'.format(b, word.synsets))
-        print('\t\t {}'.format(plant.hypernyms()))
+        plainword = [h.name().split(".")[0] for h in  plant.hypernyms()]
+        print(plainword[0])
+        return(plainword)
     else:
-        print('{} is not a word'.format(b))
+        print('{} is NOT a word'.format(askedfor))
+        return('{} is NOT a word'.format(askedfor))
+
+get_hypernym("balls")
+get_hypernym("penis")
+get_hypernym("sddfds")
+get_hypernym("shame")
+get_hypernym("pickup_truck")
+get_hypernym("african_leopard")
+get_hypernym("mailbox")
+
 
 
 """
